db302e882c142d21cecb713fd2387109c806a8db
amlingc5 <35497814+amlingc5@users.noreply.github.com>
2018-09-21 23:38:05 -0400

    Update MSP430G2553.c

--- Simple Blink/MSP430G2553.c
+++ Simple Blink/MSP430G2553.c
@@ -2,16 +2,16 @@
 
 int main(void)
 {
-  WDTCTL = WDTPW + WDTHOLD;                 // Stop watchdog timer
-  P1DIR |= 0x01;                            // Set P1.0 to output direction
+  WDTCTL = WDTPW + WDTHOLD;  // Stop watchdog timer
+  P1DIR |= 0x01;             // Set P1.0 to output direction
 
   for (;;)
   {
     volatile unsigned int i;
 
-    P1OUT ^= 0x01;                          // Toggle P1.0 using exclusive-OR
+    P1OUT ^= 0x01;           // Toggle P1.0 using exclusive-OR
 
-    i =20000;                              // Delay
+    i =20000;                // Delay
     do (i--);
     while (i != 0);
   }
