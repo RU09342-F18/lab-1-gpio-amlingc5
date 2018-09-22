bb4f0422b8566c3cfa1b391e0744bb36a9a0071f
amlingc5 <35497814+amlingc5@users.noreply.github.com>
2018-09-21 23:28:34 -0400

    Update MSP430G2553.c

--- Button Blink/MSP430G2553.c
+++ Button Blink/MSP430G2553.c
@@ -1,24 +1,24 @@
-#include <msp430.h>                         //includes header file for msp430
+#include <msp430.h>     //includes header file for msp430
 
-int main(void)                              //initialize main function
+int main(void)         
 {
-  WDTCTL = WDTPW + WDTHOLD;                 // Stop watchdog timer
-  P1DIR |= 0x01;                            // Set P1.0 to output direction
-  P1DIR &= ~BIT3;                           //Set P1.3 to input direction
-  P1REN |= BIT3;                            //Sets pull up resistor
+  WDTCTL = WDTPW + WDTHOLD;   // Stop watchdog timer
+  P1DIR |= 0x01;              // Set P1.0 to output direction
+  P1DIR &= ~BIT3;            //Set P1.3 to input direction
+  P1REN |= BIT3;             //Sets pull up resistor
 
-  int j;                                    //creates an integer
+  int j;                     //creates an integer
 
-  while(1)                                 //creates infinite while loop
+  while(1)                   //creates infinite while loop
   {
-    j = P1IN & BIT3;                         //check to see if button is pressed
-   if(j !=8)                                // if button is not pressed
+    j = P1IN & BIT3;         //check to see if button is pressed
+   if(j !=8)                 // if button is not pressed
    {
-       P1OUT ^= 0x01;                       //toggles LED
+       P1OUT ^= 0x01;       //toggles LED
    }
    else
    {
-       P1OUT &= ~BIT0;                      //turns LED off
+       P1OUT &= ~BIT0;      //turns LED off
    }
   }
 }
