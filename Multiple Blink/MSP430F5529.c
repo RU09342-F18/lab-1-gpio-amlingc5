aecc60ccabab2f0f723e4290b6e6b1d8a68a60b9
amlingc5 <35497814+amlingc5@users.noreply.github.com>
2018-09-21 23:08:53 -0400

    Update MSP430F5529.c

--- Multiple Blink/MSP430F5529.c
+++ Multiple Blink/MSP430F5529.c
@@ -5,11 +5,9 @@ PM5CTL0 &= ~LOCKLPM5 // Needed For F type devices
 
 {
     WDTCTL = WDTPW + WDTHOLD; // Stop watch dog timer
-
     P1SEL &= ~BIT0;     //Sets select bit to 0 for P1.0
     P1OUT |= BIT0;    //Sets LED in P1.0 to turn on
     P1DIR |= BIT0; //Sets Port 1 direction to output
-
     P4SEL &= ~BIT7; //Sets select bit to 0 for P4.7 to be configured as I/O
     P4OUT &= ~BIT7; //Sets LED in P4.7 to not be on
     P4DIR |= BIT7; //Sets Port 4 direction to output
@@ -19,22 +17,20 @@ PM5CTL0 &= ~LOCKLPM5 // Needed For F type devices
     i = 10000; //Sets delay for P1.0
     n = 50000; //Sets deflay for P4.7
 
-
     for (;;)
         {
-            n--;
-            i--;
+        n--;
+        i--;
 
-            if (n==0) //checks if the first variable is a high enough value to turn on the LED
+        if (n==0) //checks if the first variable is a high enough value to turn on the LED
+        {
+          P1OUT ^= BIT0; //XOR to turn on and off first LED
+          n = 50000;
+         }
+          if (i==0) //checks if the first variable is a high enough value to turn on the LED
             {
-                P1OUT ^= BIT0; //XOR to turn on and off first LED
-                n = 50000;
-            }
-
-            if (i==0) //checks if the first variable is a high enough value to turn on the LED
-            {
-                P4OUT ^= BIT7; //XOR to turn on and off second LED
-                i = 10000;
-            }
+             P4OUT ^= BIT7; //XOR to turn on and off second LED
+             i = 10000;
+         }
         }
 }
