b227de4796798debab2dabac68972b5e48b84d3a
amlingc5 <35497814+amlingc5@users.noreply.github.com>
2018-09-21 23:31:16 -0400

    Update MSPF5529LP.c

--- Button Blink/MSPF5529LP.c
+++ Button Blink/MSPF5529LP.c
@@ -4,21 +4,21 @@ int main(void)
 PM5CTL0 &= ~LOCKLPM5
 
 {
-  WDTCTL = WDTPW+WDTHOLD;                   // Stop WDT
-  P1DIR |= BIT0;                            // Set P1.0 to output direction
-  P1DIR &= ~BIT1;                           // Set P1.1 to input direction
-  P1OUT |= BIT1;                            // Set P1.1 resistor to pull-down
-  P1REN |= BIT1;                            // Set P1.1 resistor to on
-  int j;                                    // Declaring the j integer variable
+  WDTCTL = WDTPW+WDTHOLD;   // Stop WDT
+  P1DIR |= BIT0;            // Set P1.0 to output direction
+  P1DIR &= ~BIT1;           // Set P1.1 to input direction
+  P1OUT |= BIT1;            // Set P1.1 resistor to pull-down
+  P1REN |= BIT1;            // Set P1.1 resistor to on
+  int j;                    // Declaring the j integer variable
 
-  for (;;)                                  // Runs the loop indefinitely
+  for (;;)                  // Runs the loop indefinitely
   {
-      j = P1IN & BIT1;                      // Defining integer j
+      j = P1IN & BIT1;      // Defining integer j
 
-      if (j != 2)                           // Checking if P1.1 is being pressed
-          P1OUT |= BIT0;                    // Enabling the LED on P1.0
+      if (j != 2)           // Checking if P1.1 is being pressed
+          P1OUT |= BIT0;    // Enabling the LED on P1.0
       else
-          P1OUT &= ~BIT0;                   // Disabling the LED on P1.0
+          P1OUT &= ~BIT0;   // Disabling the LED on P1.0
   }
 }
 
