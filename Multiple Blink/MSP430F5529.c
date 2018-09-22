82f7ea16224f1ef911727e1ccb9249e518030be9
amlingc5 <35497814+amlingc5@users.noreply.github.com>
2018-09-21 23:06:17 -0400

    Update MSP430F5529.c

--- Multiple Blink/MSP430F5529.c
+++ Multiple Blink/MSP430F5529.c
@@ -1,12 +1,12 @@
 #include <msp430.h>
 int main(void)
 
-PM5CTL0 &= ~LOCKLPM5
+PM5CTL0 &= ~LOCKLPM5 // Needed For F type devices
 
 {
     WDTCTL = WDTPW + WDTHOLD; // Stop watch dog timer
 
-    P1SEL &= ~BIT0;     //Sets select bit to 0 for P1.0 to be configured as I/O
+    P1SEL &= ~BIT0;     //Sets select bit to 0 for P1.0
     P1OUT |= BIT0;    //Sets LED in P1.0 to turn on
     P1DIR |= BIT0; //Sets Port 1 direction to output
 
@@ -16,8 +16,8 @@ PM5CTL0 &= ~LOCKLPM5
 
     volatile unsigned int n, i;
 
-    i = 10000; //Sets delay for LED blinking in P1.0
-    n = 50000; //Sets deflay for LED blinking in P4.7
+    i = 10000; //Sets delay for P1.0
+    n = 50000; //Sets deflay for P4.7
 
 
     for (;;)
@@ -25,17 +25,16 @@ PM5CTL0 &= ~LOCKLPM5
             n--;
             i--;
 
-            if (n==0) //if statement that checks if the first variable is a high enough value to turn on the LED
+            if (n==0) //checks if the first variable is a high enough value to turn on the LED
             {
                 P1OUT ^= BIT0; //XOR to turn on and off first LED
                 n = 50000;
             }
 
-            if (i==0) //if statement that checks if the first variable is a high enough value to turn on the LED
+            if (i==0) //checks if the first variable is a high enough value to turn on the LED
             {
                 P4OUT ^= BIT7; //XOR to turn on and off second LED
                 i = 10000;
             }
         }
 }
-
